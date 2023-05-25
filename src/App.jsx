408a769f25261846d6f88a26c106535862686604
Arin Paliwal <paliwalarin09@gmail.com>
2023-05-25 14:34:36 +0530

    Added API and Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Navbar, Feed, VedioDetail, SearchFeed, ChannelDetail } from "./components"
 const App = () => {
-  return <div>App</div>;
+  return <BrowserRouter>
+    <Box sx={{ backgroundColor: "fff" }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/vedios/:id" element={<VedioDetail />} />
+        <Route path="/search/:searchText" element={<SearchFeed />} />
+      </Routes>
+
+    </Box>
+  </BrowserRouter>;
 };
 
 export default App;
---
+++ src/components/VedioDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VedioDetail = () => {
+  return (
+    <div>VedioDetail</div>
+  )
+}
+
+export default VedioDetail
