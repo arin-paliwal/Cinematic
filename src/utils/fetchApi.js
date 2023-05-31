d10e8299781679fa28ccfd037fba4ed0900fdf11
Arin Paliwal <paliwalarin09@gmail.com>
2023-05-31 13:34:00 +0530

    API Limit Handled

--- src/utils/fetchApi.js
+++ src/utils/fetchApi.js
@@ -3,7 +3,7 @@ const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 const options = {
     url: 'https://youtube-v31.p.rapidapi.com/captions',
     params: {
-        maxResults: 50,
+        maxResults: 20,
     },
     headers: {
         'X-RapidAPI-Key': 'c551de085cmsh977040e7bff3248p1a6294jsn44053bf4275a',
