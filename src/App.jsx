7335ea91862c51c8b7c09cbf355fd45ee566e056
Arin Paliwal <paliwalarin09@gmail.com>
2023-06-01 10:32:23 +0530

    Added Vercel Config

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,20 @@ import { Box } from "@mui/material";
 import { Navbar, Feed, VideoDetail, SearchFeed, ChannelDetail } from "./components";
 import Footer from "./components/Footer";
 import Error from "./components/Error";
+
 const App = () => {
   return (
     <BrowserRouter>
       <Box sx={{ backgroundColor: "#fff" }}>
         <Navbar />
         <Routes>
-          <Route path="/" exact element={<Feed />} />
+          <Route path="/" element={<Feed />} />
           <Route path="/channel/:id" element={<ChannelDetail />} />
           <Route path="/video/:id" element={<VideoDetail />} />
           <Route path="/search/:searchTerm" element={<SearchFeed />} />
-          <Route path="/t&c" element={<Error/>} />
-          <Route path="/PrivacyPolicy" element={<Error />} />
-          <Route path="FAQ" element={<Error />} />
-          <Route path="help?" element={<Error />} />
-          
+          <Route path="*" element={<Error />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </Box>
     </BrowserRouter>
   );
